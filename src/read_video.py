3a92c77d11b6ada611e0d7d6a651d69ad4ebf687
Khushi Arya <khushiarya424@gmail.com>
2026-05-04 12:30:19 +0200

    Clean-Repo

--- src/read_video.py
+++ src/read_video.py
@@ -1,35 +1,55 @@
 import cv2
-import os
+
 def play_video(video_path):
-    print("Function started...")
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
         print("Error: Cannot open video")
         return
+
+    prev_gray = None
     frame_count = 0
 
+    no_motion_ranges = []
+    start = None
 
     while True:
         ret, frame = cap.read()
-        print("Reading frame..")
-
         if not ret:
             break
-        frame_count+= 1
-        
-        print("Frame: ", frame_count, flush=True)
 
-        if frame_count % 10 == 0:
-            print("Saving to:", os.path.abspath(f"frames/frame_{frame_count}.jpg"))
-            cv2.imwrite(f"frames/frame_{frame_count}.jpg", frame)
-        
+        frame_count += 1
 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow("Video", frame)
+        if prev_gray is not None:
+            diff = cv2.absdiff(prev_gray, gray)
 
-        if cv2.waitKey(50) & 0xFF == ord('q'):
-            break
+            _, thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
+
+            thresh = cv2.GaussianBlur(thresh, (5,5), 0)
+
+            motion_score = thresh.sum()
+            print("Frame:", frame_count, "Motion:", motion_score)
+            
+
+            if motion_score < 120000000:
+                if start is None:
+                    start = frame_count
+            else:
+                if start is not None:
+                    no_motion_ranges.append((start, frame_count))
+                    start = None
+
+        prev_gray = gray
+    if start is not None:
+     no_motion_ranges.append((start, frame_count))
 
     cap.release()
-    cv2.destroyAllWindows()
+
+    # Save results
+    with open("no_motion.txt", "w") as f:
+        for s, e in no_motion_ranges:
+            f.write(f"{video_path} no-motion-frame{{{s}-{e}}}\n")
+
+    print("Done. Results saved in no_motion.txt") 
